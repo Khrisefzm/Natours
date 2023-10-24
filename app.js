719009ae8a1ecccabaf4d61571951a83e9f2330e
khrise Franchesca <khrisefzm@gmail.com>
2023-10-24 09:41:52 -0500

    add variable PORT in server

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
-app.use(morgan("dev"));
 
 //middlewares
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware");
